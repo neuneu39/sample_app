75c5b3e73ec5ee613d916bcc649e779f79516051
kan.akutsu <sakamotoshizuya@gmail.com>
2014-09-15 19:37:22 +0900

    FInish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906030243) do
+ActiveRecord::Schema.define(version: 20140915075250) do
 
 # Could not dump table "users" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
